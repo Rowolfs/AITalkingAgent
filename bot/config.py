ad59b387361a26042903813861e1ff8abcec1c86
Rowolfs <ro.volkov2018@yandex.ru>
2026-02-23 19:31:17 +0300

    fixed add token

--- bot/config.py
+++ bot/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 try:
     secret_path = os.environ.get("BOT_TOKEN_FILE","/run/secrets.env")
     with open(secret_path, "r") as f:
-        token = f.read().strip()
+        token = f.read().strip()[6:]
     print("Token loaded:", token[:4]+ "...")
     
 except FileNotFoundError:
